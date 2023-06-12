39858b4ff35a475d1d2dfcedf2e3b4740a6aa60a
abbas alme <abbasalme99@gmail.com>
2023-06-12 02:21:23 -0700

    create template

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -12,6 +12,18 @@
 */
 
 
+function coffeeTemplate(coffee){
+
+
+return `
+  <p>
+  <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+  <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc }</p>
+
+     `;
+
+}
+
 
 let myDate = new Date();
 
@@ -21,6 +33,19 @@ let today = myDate.getDay();
 
 switch(today){
 
+    case 1:
+        today = "Monday";
+        coffee = {
+        name: "Bubble Tea",
+        color: "pink",
+        pic: "images/bubble-tea.jpg",
+        alt: "A picture of Bubble Tea ",
+        day: "Monday",
+        desc: `I like me some bubble tea! `
+        }; 
+            
+   break;
+      
    case 2:
          today = "Tuesday";
     break;
@@ -33,15 +58,8 @@ switch(today){
      today = "Not sure what day it is!";
 }
 
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
-coffee = {
-    name: "Bubble Tea",
-    color: "pink",
-    pic: "images/bubble-tea.jpg",
-    alt: "A picture of Bubble Tea ",
-    day: "Wednesday",
-    desc: `I like me some bubble tea! `
-}
 
 //alert(today);
 console.log(coffee);
