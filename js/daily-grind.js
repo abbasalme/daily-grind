b04e5249fd4b7833fb41553f0e00e75fbcd3a986
abbas alme <abbasalme99@gmail.com>
2023-06-17 01:48:22 -0700

    update

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -11,18 +11,15 @@
 
 */
 
-
 function coffeeTemplate(coffee){
+    return `<p>
+            <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+                <strong class="feature">${coffee.day} Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+    
+      `;
+    }
+    
 
-
-return `
-  <p>
-  <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
-  <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc }</p>
-
-     `;
-
-}
 
 
 let myDate = new Date();
@@ -36,23 +33,38 @@ switch(today){
     case 1:
         today = "Monday";
         coffee = {
-        name: "Bubble Tea",
-        color: "pink",
-        pic: "images/bubble-tea.jpg",
-        alt: "A picture of Bubble Tea ",
-        day: "Monday",
-        desc: `I like me some bubble tea! `
+        name: "Drip",
+        color: "black",
+        pic: "images/drip.jpg",
+        alt: "A picture of drip coffee ",
+        day: "Tuesday",
+        desc: `Give me to it straight! `
         }; 
             
    break;
       
    case 2:
          today = "Tuesday";
+
+         case 1:
+            today = "Monday";
+            coffee = {
+            name: "Bubble Tea",
+            color: "pink",
+            pic: "images/bubble-tea.jpg",
+            alt: "A picture of Bubble Tea ",
+            day: "Monday",
+            desc: `I like me some bubble tea! `
+            }; 
+
+
     break;
 
     case 3:
         today = "Wednesday";
    break;
+
+    
  
    default:
      today = "Not sure what day it is!";
